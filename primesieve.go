0ab44668e1db68d68e9970e8ba3a29d85244e6e0
Caleb Lloyd <caleblloyd@gmail.com>
2016-01-11 15:28:21 -0500

    uint64; dynamic allocation

--- primesieve.go
+++ primesieve.go
@@ -19,22 +19,26 @@ const (
 	RTYPE_CHANNEL
 )
 
+// minimum number of composites to dynamically allocate
+const ALLOCATE_MIN = 1024
+
 // Generate primes using the segmented sieve of Eratosthenes.
 // This algorithm uses O(n log log n) operations and O(sqrt(n)) space.
-func SegmentedSieve(rType int, rLimit int, rChan chan int) (int, []int) {
-	var rList []int
-	var rInt int
+func SegmentedSieve(rType uint8, rLimit uint64, rChan chan uint64) (uint64, []uint64) {
+	var rList []uint64
+	var rInt uint64
+	var isComposite []bool
 
-	var limit int
+	var limit uint64
 	if rType == RTYPE_LIST_N || rType == RTYPE_N || rType == RTYPE_CHANNEL {
-		limit = math.MaxInt32
+		limit = math.MaxUint64
 	} else {
 		limit = rLimit
 	}
 
-	var count int
+	var count uint64
 	// anonymous function to handle different return types
-	foundContinue := func(n int) bool {
+	foundContinue := func(n uint64) bool {
 		count++
 		if rType == RTYPE_COUNT {
 			rInt = count
@@ -55,27 +59,16 @@ func SegmentedSieve(rType int, rLimit int, rChan chan int) (int, []int) {
 		return rInt, rList
 	}
 
-	sqrt := int(math.Sqrt(float64(limit)))
 	var sieve [SEGMENT_SIZE]bool
 
-	// generate small primes <= sqrt
-	var isComposite = make([]bool, sqrt+1)
-	for i := 2; i*i <= sqrt; i++ {
-		if !isComposite[i] {
-			for j := i * i; j <= sqrt; j += i {
-				isComposite[j] = true
-			}
-		}
-	}
-
-	s := 1
-	n := 3
-	var wheel2357 = []int{2, 4, 2, 4, 6, 2, 6, 4, 2, 4, 6, 6, 2, 6, 4, 2, 6, 4, 6, 8, 4, 2, 4, 2, 4, 8, 6, 4, 6, 2, 4, 6, 2, 6, 6, 4, 2, 4, 6, 2, 6, 4, 2, 4, 2, 10, 2, 10}
-	var wheelPos int
-	var primes, next []int
+	s := uint64(2)
+	n := uint64(3)
+	var wheel2357 = []uint8{2, 4, 2, 4, 6, 2, 6, 4, 2, 4, 6, 6, 2, 6, 4, 2, 6, 4, 6, 8, 4, 2, 4, 2, 4, 8, 6, 4, 6, 2, 4, 6, 2, 6, 6, 4, 2, 4, 6, 2, 6, 4, 2, 4, 2, 10, 2, 10}
+	var wheelPos uint8
+	var primes, next []uint64
 	var stopIteration bool
 
-	for low := 0; low <= limit; low += SEGMENT_SIZE {
+	for low := uint64(0); low <= limit; low += SEGMENT_SIZE {
 
 		for i, _ := range sieve {
 			sieve[i] = true
@@ -87,11 +80,40 @@ func SegmentedSieve(rType int, rLimit int, rChan chan int) (int, []int) {
 			high = limit
 		}
 
-		// store small primes needed to cross off multiples
-		for ; s*s <= high; s++ {
-			if !isComposite[s] {
-				primes = append(primes, s)
-				next = append(next, s*s-low)
+		sqrt := uint64(math.Sqrt(float64(high))) + 1
+		if s < sqrt {
+			// dynamically allocate isComposite
+			oldLen := uint64(len(isComposite))
+			allocate := int64(sqrt - oldLen)
+			if allocate > 0 {
+				if allocate < ALLOCATE_MIN {
+					allocate = ALLOCATE_MIN
+				}
+
+				// backfill new allocation
+				isComposite = append(isComposite, make([]bool, allocate)...)
+				sqrt = uint64(len(isComposite))
+
+				for i := uint64(0); i < uint64(len(primes)); i++ {
+					p := primes[i] * primes[i]
+					if p < oldLen {
+						p = oldLen - (oldLen % primes[i]) + primes[i]
+					}
+					for ; p < sqrt; p += primes[i] {
+						isComposite[p] = true
+					}
+				}
+			}
+
+			// find new small primes
+			for ; s < sqrt; s++ {
+				if !isComposite[s] {
+					primes = append(primes, s)
+					next = append(next, s*s-low)
+					for j := s * s; j < sqrt; j += s {
+						isComposite[j] = true
+					}
+				}
 			}
 		}
 
@@ -117,7 +139,7 @@ func SegmentedSieve(rType int, rLimit int, rChan chan int) (int, []int) {
 			}
 			// wheel factorization optimization
 			if n >= 11 {
-				n += wheel2357[wheelPos]
+				n += uint64(wheel2357[wheelPos])
 				wheelPos++
 				if wheelPos >= WHEEL_LEN {
 					wheelPos = 0
@@ -140,31 +162,31 @@ func SegmentedSieve(rType int, rLimit int, rChan chan int) (int, []int) {
 
 }
 
-func ListN(numPrimes int) []int {
+func ListN(numPrimes uint64) []uint64 {
 	_, l := SegmentedSieve(RTYPE_LIST_N, numPrimes, nil)
 	return l
 }
 
-func ListMax(max int) []int {
+func ListMax(max uint64) []uint64 {
 	_, l := SegmentedSieve(RTYPE_LIST_MAX, max, nil)
 	return l
 }
 
-func Count(max int) int {
+func Count(max uint64) uint64 {
 	s, _ := SegmentedSieve(RTYPE_COUNT, max, nil)
 	return s
 }
 
-func PrimeN(n int) int {
+func PrimeN(n uint64) uint64 {
 	s, _ := SegmentedSieve(RTYPE_N, n, nil)
 	return s
 }
 
-func PrimeMax(max int) int {
+func PrimeMax(max uint64) uint64 {
 	s, _ := SegmentedSieve(RTYPE_MAX, max, nil)
 	return s
 }
 
-func Channel(c chan int) {
+func Channel(c chan uint64) {
 	go SegmentedSieve(RTYPE_CHANNEL, 0, c)
 }
--- primesieve_test.go
+++ primesieve_test.go
@@ -2,7 +2,9 @@ package primesieve
 
 import (
 	"fmt"
+	"math"
 	"testing"
+	"time"
 )
 
 func BenchmarkPrintPrimes(b *testing.B) {
@@ -14,14 +16,14 @@ func BenchmarkPrintPrimes(b *testing.B) {
 func TestSumPrimes1000(t *testing.T) {
 	const PSUM = 3682913
 	const THOUSANDTH_PRIME = 7919
-	var calcsum int
+	var calcsum uint64
 	primes := ListN(1000)
 	for _, p := range primes {
 		calcsum += p
 	}
 	fmt.Println(primes[len(primes)-1])
 	if PSUM != calcsum {
-		t.Errorf("expected %d sum of GenPrimes(1000), got %d\n", PSUM, calcsum)
+		t.Errorf("expected %d sum of ListN(1000), got %d\n", PSUM, calcsum)
 	}
 
 	primes = ListMax(THOUSANDTH_PRIME)
@@ -31,12 +33,31 @@ func TestSumPrimes1000(t *testing.T) {
 	}
 	fmt.Println(primes[len(primes)-1])
 	if PSUM != calcsum {
-		t.Errorf("expected %d sum of MaxGenPrimes(1000), got %d\n", PSUM, calcsum)
+		t.Errorf("expected %d sum of ListMax(1000), got %d\n", PSUM, calcsum)
 	}
 
 	tp := PrimeN(1000)
 	fmt.Println(tp)
 	if tp != THOUSANDTH_PRIME {
-		t.Errorf("expected %d for NthGetPrime(1000), got %d\n", THOUSANDTH_PRIME, tp)
+		t.Errorf("expected %d for PrimeN(1000), got %d\n", THOUSANDTH_PRIME, tp)
 	}
+}
+
+func TestPerformance(t *testing.T) {
+	bm := func(max uint64, s string, expected uint64) {
+		start := time.Now()
+		c := Count(max)
+		elapsed := time.Since(start)
+		fmt.Printf("%s count: %d time: %s", s, c, elapsed)
+		fmt.Println()
+		if c != expected {
+			t.Errorf("expected %d for Count(%d), got %d\n", expected, max, c)
+		}
+	}
+
+	bm(uint64(math.Pow(10, 7)), "10^7", 664579)
+	bm(uint64(math.Pow(10, 8)), "10^8", 5761455)
+	bm(uint64(math.Pow(10, 9)), "10^9", 50847534)
+	bm(uint64(math.Pow(2, 31))-1, "2^31-1", 105097565)
+	bm(uint64(math.Pow(2, 32)), "2^32", 203280221)
 }
